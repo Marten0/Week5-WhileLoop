f64eda364dafb81e225d81033e9a14c08d2d063c
Opilane TTHK <opilane@tthk.ee>
2021-09-29 09:51:15 +0300

    WhileLoop

---
+++ Numberguesser/Numberguesser/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace Numberguesser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetAppInfo();
+            GreetUser();
+
+            
+
+            while (true) {
+
+                //int correctNumber = 7;
+
+                Random random = new Random();
+
+                init correctNumber = random.Next(1, 11);
+
+                int guess = 0;
+
+                Console.WriteLine("Guess a number between 1 and 10");
+
+                while (guess != correctNumber)
+                {
+                    string input = Console.ReadLine();
+
+                    if (!int.TryParse(input, out guess))
+                    {
+                        
+                      
+                        continue;
+                    }
+
+                    guess = Int32.Parse(input);
+
+                    if (guess != correctNumber)
+                    {
+
+
+                        PrintColorMessage(ConsoleColor.Red, "Wrong number, please try again");
+                    }
+                }
+                Console.ForegroundColor = ConsoleColor.Yellow;
+
+                Console.WriteLine("You are CORRECT!!!);
+    
+
+                Console.ResetColor();
+
+                Console.WriteLine("Play again [Y or N]");
+
+                string answer = Console.ReadLine().ToUpper();
+
+                if (answer == "Y")
+                {
+                    continue;
+                }
+                else if (answer == "N")
+                {
+
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+        static void GetAppInfo()
+        {
+            string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "Marten";
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
+
+            Console.ResetColor();
+        }
+        static void GreetUser()
+        {
+            Console.WriteLine("What is your name?");
+
+            string inputName = Console.ReadLine();
+
+            Console.WriteLine("Hello {0}, lets play a game..", inputName);
+        }
+        static void PrintColorMessage(ConsoleColor color, string message)
+        {
+            Console.ForegroundColor = color;
+
+            Console.WriteLine("Please enter an acutal number");
+
+            Console.ResetColor();
+
+        }
+        //52:26
+    }
+}
---
+++ WhileLoop/PINValidationThreeTries/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PINValidationThreeTries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajala sisestada PIN-koodi
+            //programm võrdleb sisestatud PIN-koodi arvuga 1234
+            // kui sisestatud PIN-kood on 1234
+            //programm kuvab konsoolis "Tere tulemast!"
+            //kui sisestatud PIN on vale, programm kuvab konsoolis
+            //"Vale PIN.Proovi uuesti."
+            //kasutajal on kolm katset
+
+            int i = 0;
+
+            while(i < 3)
+            {
+                Console.WriteLine("Sisesta PIN:");
+                int userPIN = Convert.ToInt32(Console.ReadLine());
+
+                if (userPIN == 1234)
+                {
+                    Console.WriteLine("Tere tulemast");
+                    i = 3;
+                }
+                else
+                {
+                    i = i + 1;
+                    Console.WriteLine($"Vale PIN. {3 - i} katset on jäänud.");
+                }
+            }
+        }
+    }
+}
